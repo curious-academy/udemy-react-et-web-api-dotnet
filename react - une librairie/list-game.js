e2b3dd78e622d76faa16aee233c834d09c0fd5ae
Evan BOISSONNOT <evan@boissonnot.fr>
2023-11-02 17:45:27 +0100

    feat: fin quete 004

---
+++ react - une librairie/list-game.js
@@ -0,0 +1,26 @@
+const ListGame = () => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        Id
+                    </th>
+                    <th>
+                        Gagnée ?
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        1
+                    </td>
+                    <td>
+                        Success
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
---
+++ react - une librairie/main.js
@@ -0,0 +1,4 @@
+const root = document.getElementById('root');
+const domRoot = ReactDOM.createRoot(root);
+
+domRoot.render(<div><h1>Bienvenue !</h1><ListGame></ListGame></div>) // jsx, sucre syntaxique
